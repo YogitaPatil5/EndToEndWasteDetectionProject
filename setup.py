4238f25ef823b37c482fcf4955ed41e43de5442c
Yogita Patil <147682846+YogitaPatil5@users.noreply.github.com>
2025-01-16 14:14:23 +0530

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Yogita Patil',
+    author_email= 'yogitarajput.04@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
